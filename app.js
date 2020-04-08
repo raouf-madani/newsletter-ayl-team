d90c6a4fdb14ed41e1063fd328eb1cfd181c48ba
Raouf Madani <raoufmadani@live.fr>
2020-04-08 13:38:17 +0100

    Newsletter V1 update

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.post('/',function(req,res){
         res.sendFile(__dirname+'/success.html');
     }else{
         res.sendFile(__dirname+'/failure.html');
+        console.log(response.statusMessage);
     };
 
     response.on("data",function(data){
